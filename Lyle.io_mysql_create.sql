fac33ef3aec2f543f294e395c1a183f748f86041
Noah Schweser <nschweser@smu.edu>
2018-11-10 14:56:18 -0600

    Update Lyle.io_mysql_create.sql

--- Lyle.io_mysql_create.sql
+++ Lyle.io_mysql_create.sql
@@ -3,7 +3,7 @@ create database LyleIO;
 USE LyleIO;
 
 drop table if exists Users;
-CREATE TABLE Users (
+CREATE TABLE users (
 	userName varchar(30) UNIQUE DEFAULT 'user',
 	pass varchar(30) NOT NULL DEFAULT 'password',
 	track varchar(20),
@@ -22,14 +22,14 @@ CREATE TABLE forums (
 );
 
 drop table if exists Posts;
-CREATE TABLE Posts (
+CREATE TABLE posts (
 	postID int NOT NULL AUTO_INCREMENT,
 	title varchar(128) NOT NULL DEFAULT 'New Post',
 	body varchar(1024) NOT NULL DEFAULT 'Body',
 	forumID int NOT NULL,
 	userID int NOT NULL,
 	PRIMARY KEY (postID),
-	FOREIGN KEY (userID) REFERENCES Users(userID),
+	FOREIGN KEY (userID) REFERENCES users(userID),
 	FOREIGN KEY(forumID) REFERENCES forums(forumID)
 );
 
@@ -40,7 +40,7 @@ CREATE TABLE messages (
 	body varchar(512) NOT NULL,
 	userID int NOT NULL,
 	PRIMARY KEY (msgID),
-	FOREIGN KEY (userID) REFERENCES Users(userID),
-	FOREIGN KEY(postID) REFERENCES Posts(postID)
+	FOREIGN KEY (userID) REFERENCES users(userID),
+	FOREIGN KEY(postID) REFERENCES posts(postID)
 );
 

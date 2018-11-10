a775f3c287e3cac2435d8bd9a0871ebcd2f2cc81
Ahmed Hadi <ahadi@smu.edu>
2018-11-10 16:48:26 -0600

    first push for the app

--- lyleio/src/app/app.module.ts
+++ lyleio/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
+import { HeaderComponent } from './header/header.component';
+import { ChatBoardComponent } from './chat-board/chat-board.component';
+import { CommentsComponent } from './chat-board/comments/comments.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    HeaderComponent,
+    ChatBoardComponent,
+    CommentsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
